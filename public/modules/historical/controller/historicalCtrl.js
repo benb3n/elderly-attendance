0dbc4f59a70e5068b2ce9f07ec0e9d2f35309fb5
Benedict Wee <benedictwee.2014@sis.smu.edu.sg>
2017-12-16 03:14:12 +0800

    Fix UI for realtime page

--- public/modules/historical/controller/historicalCtrl.js
+++ public/modules/historical/controller/historicalCtrl.js
@@ -1,6 +1,12 @@
 angular.module('HistoricalCtrl', [])
 .controller('HistoricalController', ['$scope', '$routeParams', function ($scope, $routeParams) {
     $(document).ready(function() {
+        $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            }
+        );
         $('select').material_select();
         $('.datepicker').pickadate({
             selectMonths: true, // Creates a dropdown to control month
--- public/modules/real-time/controller/real-timeCtrl.js
+++ public/modules/real-time/controller/real-timeCtrl.js
@@ -1,11 +1,15 @@
 angular.module('RealTimeCtrl', [])
 .controller('RealTimeController', ['$scope', '$routeParams', function ($scope, $routeParams) {
     $(document).ready(function() {
+        // TABS
+        $('ul.tabs').tabs();
+        $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            }
+        );
         $('select').material_select();
-        $('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15 // Creates a dropdown of 15 years to control year
-        });
     });
     var vm = this;
 
