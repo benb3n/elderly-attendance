d4dca9508dbc0b5d06abf58d85aeb7af8b5b671a
Tee, Shi Hui <shihui.tee.2014@smu.edu.sg>
2018-02-21 10:49:38 +0800

    historical activity, fixed truncation of box heatmap chart in mobile view

--- public/modules/historical/directive/historicalDirective.js
+++ public/modules/historical/directive/historicalDirective.js
@@ -1018,7 +1018,7 @@ angular.module('HistoricalDirective', [])
 
             var svg = d3.select(Element[0]).append("svg")
               .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
+              .attr("height", height + margin.top + margin.bottom + hMargin)
               .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
